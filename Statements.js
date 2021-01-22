1e5b4e37fa48ea6ee843358740de160a2f572975
Dzmitry Abramuk <Abramuk08@mail.ru>
2021-01-22 10:41:15 +0300

    Statements

---
+++ Statements.js
@@ -0,0 +1,29 @@
+let str1 = 'Hello World';
+let str2 = 'HELLO World';
+
+if (str1 == str2) {
+console.log ('Строки равны');}
+else {
+console.log ('Строки не равны');
+}
+	
+let int1 = 4;
+let int2 = 6;
+
+if (int1 == int2) {
+console.log ('Числа равны');}
+else { if (int1 > int2) {
+console.log ('Первое число больше второго');}
+else {
+console.log ('Первое число меньше второго');
+}
+}
+
+let bool1 = (+'10' > 5);
+let bool2 = true;
+
+if (bool1 == bool2) {
+	console.log ('Равны'); 
+} else {
+	console.log ('Не равны'); 
+}
