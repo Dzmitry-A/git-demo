7c77a0c1b92fc40f1d409981ab48a2b92d066edf
Dzmitry Abramuk <Abramuk08@mail.ru>
2021-01-22 13:06:11 +0300

    Logical Operators

---
+++ LogicalOperators.js
@@ -0,0 +1,17 @@
+let age = 19;
+
+if (age >= 14 && age <= 90) {
+console.log ('Выполняется') 
+}
+
+if (!age >= 14 && !age <= 90) {
+console.log ('Выполняется') 
+} else {
+console.log ('Не выполняется')
+}
+
+if (age <= 14 && age >= 90) {
+console.log ('Выполняется') 
+} else {
+console.log ('Не выполняется')
+}
